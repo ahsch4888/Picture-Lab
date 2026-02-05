c2c69b380897893951e5060ef55ad6a1db44d4cb
ShadoeSlash <127456530+ShadoeSlash@users.noreply.github.com>
2026-02-05 09:42:55 -0800

    2/5/26 update
    Completed:
    
    testKeepOnlyBlue
    testKeepOnlyRed
    tesetKeepOnlyGreen
    testNegate
    testGrayscale
    
    To do:
    testEdgeDetection
    testFixUnderwater
    testChromakey
    testEncodeAndDecode
    testGetCoutnRedOverValue
    testSetRedToHalfValueInTopHalf
    testGetAverageForColumn
    
    edgeDetection

--- classes/PictureTester.java
+++ classes/PictureTester.java
@@ -1,37 +1,117 @@
 /**
  * This class contains class (static) methods
- * that will help you test the Picture class 
- * methods.  Uncomment the methods and the code
+ * that will help you test the Picture class
+ * methods. Uncomment the methods and the code
  * in the main to test.
  * 
- * @author Barbara Ericson 
+ * @author Barbara Ericson
  */
-public class PictureTester
-{
-  /** Method to test zeroBlue */
-  public static void testZeroBlue()
-  {
-    Picture beach = new Picture("images/beach.jpg");
-    //.explore will work on download/VS Code. Not currently set up to use on Codespaces
-    //beach.explore();  
-    beach.zeroBlue();
-    //beach.explore();
-    //write is set up by default to work on Codespaces
-    beach.write("images/oldBeach.jpg");
-  }
-  
-  
-  /** Main method for testing.  Every class can have a main
-    * method in Java */
-  public static void main(String[] args)
-  {
-    // uncomment a call here to run a test
-    // and comment out the ones you don't want
-    // to run
-    testZeroBlue();
-    //testKeepOnlyBlue();
-    //testKeepOnlyRed();
-    //testNegate();
-  
-  }
+public class PictureTester {
+    public static void main(String[] args) {
+
+        // testZeroBlue("images/beach.jpg");
+        // testKeepOnlyRed("images/beach.jpg");
+        // testKeepOnlyGreen("images/beach.jpg");
+        // testKeepOnlyBlue("images/beach.jpg");
+        // testNegate("images/beach.jpg");
+        // testGrayscale("images/beach.jpg");
+        testEdgeDetection("images/swan.jpg", 10);
+    }
+
+    public static void testZeroRed(String fileName) {
+        Picture image = new Picture(fileName);
+        // image.explore();
+        image.zeroRed();
+        // image.explore();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testZeroGreen(String fileName) {
+        Picture image = new Picture(fileName);
+        // image.explore();
+        image.zeroGreen();
+        // image.explore();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testZeroBlue(String fileName) {
+        Picture image = new Picture(fileName);
+        // image.explore();
+        image.zeroBlue();
+        // image.explore();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testKeepOnlyRed(String fileName) {
+        Picture image = new Picture(fileName);
+        // image.explore();
+        image.zeroBlue();
+        image.zeroGreen();
+        // image.explore();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testKeepOnlyGreen(String fileName) {
+        Picture image = new Picture(fileName);
+        // image.explore();
+        image.zeroRed();
+        image.zeroBlue();
+        // image.explore();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testKeepOnlyBlue(String fileName) {
+        Picture image = new Picture(fileName);
+        // image.explore();
+        image.zeroRed();
+        image.zeroGreen();
+        // image.explore();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testNegate(String fileName) {
+        Picture image = new Picture(fileName);
+        image.negate();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testGrayscale(String fileName) {
+        Picture image = new Picture(fileName);
+        image.grayscale();
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
+    public static void testEdgeDetection(String fileName, int edgeDist) {
+        Picture image = new Picture(fileName);
+        image.edgeDetection(edgeDist);
+        image.write(
+                fileName.substring(0, fileName.indexOf(".jpg")) +
+                        "-modified" +
+                        ".jpg");
+    }
+
 }
